34d8e2473df87dc4b56b18308bef6b627b5c998d
Tiago Dinis <tiagodinis33@gmail.com>
2022-07-17 14:46:48 +0000

    Add markdown editor

---
+++ lib/widget/markdown_editor.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:tabnews/markdown.dart';
+
+class MarkdownEditor extends StatefulWidget {
+  const MarkdownEditor({Key? key}) : super(key: key);
+  @override
+  State<MarkdownEditor> createState() => MarkdownEditorState();
+}
+
+class MarkdownEditorState extends State<MarkdownEditor> {
+  final textController = TextEditingController();
+  int selectedTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15.0),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(15.0),
+              ),
+              child: NavigationBar(
+                height: 60,
+                selectedIndex: selectedTab,
+                onDestinationSelected: (value) =>
+                    setState(() => selectedTab = value),
+                destinations: const [
+                  NavigationDestination(
+                    label: "Edit",
+                    icon: Icon(Icons.edit),
+                  ),
+                  NavigationDestination(
+                    icon: Icon(Icons.preview),
+                    label: "Preview",
+                  ),
+                ],
+              ),
+            ),
+          ),
+          (selectedTab == 0
+              ? Padding(
+                  padding: const EdgeInsets.only(
+                    right: 8.0,
+                    left: 8,
+                    top: 0,
+                    bottom: 18,
+                  ),
+                  child: TextField(
+                    controller: textController,
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    decoration: null,
+                  ),
+                )
+              : Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SingleChildScrollView(
+                      child: Markdown(textController.text),
+                    ),
+                  ),
+                )),
+        ],
+      ),
+    );
+  }
+}
